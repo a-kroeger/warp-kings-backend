59d41510885ccefd937c527b8e176935a4fec085
Alex Kroeger <alex@detourdigital.ca>
2021-03-21 19:39:09 -0600

    Init

--- config/plugins.js
+++ config/plugins.js
@@ -1,16 +1,16 @@
 module.exports = ({ env }) => ({
-    // ...
-    upload: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env('detour-digital'),
-        api_key: env('721317217541439'),
-        api_secret: env('mC1bALOQIkDVhkzJ__nesspbU_A'),
-      },
-      actionOptions: {
-        upload: {},
-        delete: {},
-      },
+  // ...
+  upload: {
+    provider: 'cloudinary',
+    providerOptions: {
+      cloud_name: env('CLOUDINARY_NAME'),
+      api_key: env('CLOUDINARY_KEY'),
+      api_secret: env('CLOUDINARY_SECRET'),
     },
-    // ...
-  });
+    actionOptions: {
+      upload: {},
+      delete: {},
+    },
+  },
+  // ...
+});
