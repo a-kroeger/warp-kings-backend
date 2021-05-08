d5d4ef4933264c137ad520bc3d5eb15d3f087be9
Alex Kroeger <alex@detourdigital.ca>
2021-05-08 13:15:21 -0600

    Set .env variables to hide credentials

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.zg7kg.mongodb.net'),
+        host: env('DATABASE_HOST', process.env.HOST),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'warpKingsMongoDbStrapi'),
-        username: env('DATABASE_USERNAME', 'WkAdmin'),
-        password: env('DATABASE_PASSWORD', 'zsmPQ@B$2HB%U0$'),
+        database: env('DATABASE_NAME', process.env.DATABASE),
+        username: env('DATABASE_USERNAME', process.env.DB_USER),
+        password: env('DATABASE_PASSWORD', process.env.DB_PW),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
