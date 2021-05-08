4068e8934bb8ad3916a7b97b99ad29459f74f5fd
Alex Kroeger <alex@detourdigital.ca>
2021-05-08 13:38:48 -0600

    Changed cloud_name

--- extensions/upload/config/settings.js
+++ extensions/upload/config/settings.js
@@ -1,7 +1,7 @@
 module.exports = {
     "provider": "cloudinary",
     "providerOptions": {
-        "cloud_name": "senior-hub",
+        "cloud_name": "detour-digital",
         "api_key": process.env.CLOUDINARY_API_KEY,
         "api_secret": process.env.CLOUDINARY_SECRET
     }
