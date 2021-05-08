384f77c3097e4566a6bd15dff69e919cec9a70b8
Alex Kroeger <alex@detourdigital.ca>
2021-05-08 17:37:10 -0600

    Installed dependencies for cloudinary

--- extensions/upload/config/settings.js
+++ extensions/upload/config/settings.js
@@ -2,7 +2,7 @@ module.exports = {
     "provider": "cloudinary",
     "providerOptions": {
         "cloud_name": "detour-digital",
-        "api_key": process.env.CLOUDINARY_API_KEY,
-        "api_secret": process.env.CLOUDINARY_SECRET
+        "api_key": '721317217541439',
+        "api_secret": 'mC1bALOQIkDVhkzJ__nesspbU_A'
     }
 };
